081b7f70fbbc8568b989514c7fa34f9e7edbb195
9alile1 <9alile1@gmail.com>
2020-04-13 16:35:57 +0300

    Reseting the project from ground

--- routes/api.js
+++
@@ -1,7 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let image_Controller = require('../controllers/imageController');
-
-router.get('/', image_Controller.show_images);
-
-module.exports = router;
